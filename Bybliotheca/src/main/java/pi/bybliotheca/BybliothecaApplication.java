ac8c8429cffc9fe827de0e8430350e00c6f29530
AnaBlascoParra <ana.21guns@gmail.com>
2023-05-10 20:39:45 +0200

    first commit

---
+++ Bybliotheca/src/main/java/pi/bybliotheca/BybliothecaApplication.java
@@ -0,0 +1,13 @@
+package pi.bybliotheca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BybliothecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BybliothecaApplication.class, args);
+	}
+
+}
---
+++ Bybliotheca/src/test/java/pi/bybliotheca/BybliothecaApplicationTests.java
@@ -0,0 +1,13 @@
+package pi.bybliotheca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BybliothecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
