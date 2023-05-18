56bac08bc7dbf2033d243234a16908192ad30ed9
AnaBlascoParra <ana.21guns@gmail.com>
2023-05-18 16:21:05 +0200

    prog menu front

--- bybliotheca_flutter/lib/main.dart
+++ bybliotheca_flutter/lib/main.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 
+
+
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Bybliotheca',
-      theme: ThemeData(
-        primarySwatch: Colors.brown,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Bybliotheca'),
+        ),
+        
       ),
-      home: ListBooks(),
     );
   }
 }
