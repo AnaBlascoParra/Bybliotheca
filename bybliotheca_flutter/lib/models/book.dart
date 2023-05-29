7abd0d9869e86a29999e83c9f8966a01f243b2a8
AnaBlascoParra <ana.21guns@gmail.com>
2023-05-29 22:32:25 +0200

    models

--- bybliotheca_flutter/lib/models/book.dart
+++ bybliotheca_flutter/lib/models/book.dart
@@ -1,5 +1,11 @@
+import 'dart:convert';
+
+Book bookJson(String str) => Book.fromJson(json.decode(str));
+
+String bookToJson(Book data) => json.encode(data.toJson());
+
 class Book {
-  int id;
+  int? id;
   String title;
   String author;
   String summary;
@@ -8,28 +14,31 @@ class Book {
   int year;
   int qty;
 
-  Book({
-    required this.id,
-    required this.title,
-    required this.author,
-    required this.summary,
-    required this.genre,
-    required this.npages,
-    required this.year,
-    required this.qty,
-});
+  Book(
+      {required this.title,
+      required this.author,
+      required this.summary,
+      required this.genre,
+      required this.npages,
+      required this.year,
+      required this.qty});
 
-  factory Book.fromJson(Map<String, dynamic> json) {
-    return Book(
-      id: json['id'],
+  factory Book.fromJson(Map<String, dynamic> json) => Book(
       title: json['title'],
       author: json['author'],
       summary: json['summary'],
       genre: json['genre'],
       npages: json['npages'],
       year: json['year'],
-      qty: json['qty'],
-    );
-  }
-  
+      qty: json['qty']);
+
+  Map<String, dynamic> toJson() => {
+        "title": title,
+        "author": author,
+        "summary": summary,
+        "genre": genre,
+        "npages": npages,
+        "year": year,
+        "qty": qty
+      };
 }
--- bybliotheca_flutter/lib/models/user.dart
+++ bybliotheca_flutter/lib/models/user.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+User userJson(String str) => User.fromJson(json.decode(str));
+
 class User {
   int? id;
   String username;
@@ -7,17 +11,28 @@ class User {
   String name;
   String surname;
 
-  User(this.username, this.dni, this.email, this.password, this.name,
-      this.surname);
+  User(
+      {required this.username,
+      required this.dni,
+      required this.email,
+      required this.password,
+      required this.name,
+      required this.surname});
 
-  // factory User.fromJson(Map<String, dynamic> json) {
-  //   return User(
-  //       id: json['id'],
-  //       username: json['username'],
-  //       dni: json['dni'],
-  //       email: json['email'],
-  //       password: json['password'],
-  //       name: json['name'],
-  //       surname: json['surname']);
-  // }
+  factory User.fromJson(Map<String, dynamic> json) => User(
+      username: json['username'],
+      dni: json['dni'],
+      email: json['email'],
+      password: json['password'],
+      name: json['name'],
+      surname: json['surname']);
+
+  Map<String, dynamic> toJson() => {
+        "username": username,
+        "dni": dni,
+        "email": email,
+        "password": password,
+        "name": name,
+        "surname": surname
+      };
 }
